80b62fd440dfa6b7df09bbf342594591fbd90b0f
gregolly <gregollyff@hotmail.com>
2022-06-22 13:55:53 -0300

    Teste com consumo de api do github para retornar dados do usuario

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
 
 app.listen("3000");
 
-// app.use(express.json())
-
-let author = "Jaqueline"
-
-// app.route('/').get((req, res) => res.send(author))
-
-// app.route("/").put((req, res) => {
-//     author = req.body.author;
-//     res.send(author)
-// })
-
-
-app.route('/:identificador').delete((req, res) => {
-    res.send(req.params.identificador);
-})
+app.route("/").get((req, res) => {
+    axios.get("https://api.github.com/users/gregolly")
+    .then(result => res.send(`<img src=${result.data.avatar_url} />`))
+    .catch(error => console.log(error))
+});
